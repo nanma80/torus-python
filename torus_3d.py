9eaa608f7ec654f9df8c6f7f5cdcc6634d1abaa4
Nan Ma <nma@c02jl01kdkq4.guest.corp.microsoft.com>
2017-10-06 14:19:31 -0700

    all 4 branches

--- torus_3d.py
+++ torus_3d.py
@@ -1,49 +1,65 @@
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+# from stl import mesh
 
-# Generate torus mesh
-angle = np.linspace(0, 2 * np.pi, 32)
+def get_torus_surface(is_circle, phase):
+  epsilon = 0.1
 
-epsilon = 0.1
-limit = 2
+  u_range = np.linspace(epsilon, np.pi / 2 - epsilon, 32)
+  v_range = np.linspace(0, 2 * np.pi, 32)
+  u, v = np.meshgrid(u_range, v_range)
+  r, R = 0.5, 1
 
-u_range = np.linspace(epsilon, np.pi / 2 - epsilon, 16)
-v_range = np.linspace(0, 2 * np.pi, 32)
+  if is_circle:
+    rho = np.sin(epsilon) / np.minimum(np.sin(u), np.cos(u))
+  else:
+    rho = 1
 
-u, v = np.meshgrid(u_range, angle)
-r, R = 0.5, 1
+  x = rho * np.cos(u + v + phase)
+  y = rho * np.sin(u + v + phase)
 
-x_circle = np.cos(u + v)
-y_circle = np.sin(u + v)
-
-X0 = (R + r * x_circle) * np.cos(v)
-Y0 = (R + r * x_circle) * np.sin(v)
-Z0 = r * y_circle
-
-fig = plt.figure()
-ax = fig.gca(projection = '3d')
-
-ax.set_xlim3d(-limit, limit)
-ax.set_ylim3d(-limit, limit)
-ax.set_zlim3d(-limit, limit)
-
-rho = np.sin(epsilon) / np.minimum(np.sin(u), np.cos(u))
-# rho = np.sin(epsilon) / np.sin(u)
-
-x_line = rho * np.cos(u + v)
-y_line = rho * np.sin(u + v)
-
-X1 = (R + r * x_line) * np.cos(v)
-Y1 = (R + r * x_line) * np.sin(v)
-Z1 = r * y_line
-
-X = np.concatenate((X0, X1))
-Y = np.concatenate((Y0, Y1))
-Z = np.concatenate((Z0, Z1))
-
-ax.plot_surface(X, Y, Z, color = 'w', rstride = 1, cstride = 1)
+  X = (R + r * x) * np.cos(v)
+  Y = (R + r * x) * np.sin(v)
+  Z = r * y
+  return X, Y, Z
 
 
-# ax.set_aspect('equal')
-plt.show()
+def get_branch(phase):
+  X0, Y0, Z0 = get_torus_surface(True, phase)
+  X1, Y1, Z1 = get_torus_surface(False, phase)
+  X = np.concatenate((X0, X1))
+  Y = np.concatenate((Y0, Y1))
+  Z = np.concatenate((Z0, Z1))
+  return X, Y, Z
+
+
+def get_branches():
+  X, Y, Z = None, None, None
+  for i in xrange(4):
+    X1, Y1, Z1 = get_branch(i * np.pi / 2)
+    if X is None:
+      X, Y, Z = X1, Y1, Z1
+    else:
+      X = np.concatenate((X, X1))
+      Y = np.concatenate((Y, Y1))
+      Z = np.concatenate((Z, Z1))
+  return X, Y, Z
+
+
+def plot():
+  limit = 1.2
+  fig = plt.figure()
+  ax = fig.gca(projection='3d')
+  ax.set_xlim3d(-limit, limit)
+  ax.set_ylim3d(-limit, limit)
+  ax.set_zlim3d(-limit, limit)
+
+  X, Y, Z = get_branches()
+
+  ax.plot_surface(X, Y, Z, color='w', rstride=1, cstride=1)
+  ax.set_aspect('equal')
+  plt.show()
+
+
+plot()
